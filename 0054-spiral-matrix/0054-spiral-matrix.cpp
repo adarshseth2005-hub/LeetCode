4b3aa08434d86a6889457ae6aac2fc7de09f327f
Adarsh Seth <adarshofficial918@gmail.com>
2026-08-11 12:51:29 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.67%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& arr) {
-        int minr = 0, maxr = arr.size()-1, minc = 0, maxc = arr[0].size()-1;
+        int minr =0 , minc = 0, maxr = arr.size()-1, maxc = arr[0].size()-1;
+
         vector<int> ans;
         while(minr<=maxr && minc<=maxc){
-            for(int i = minc ; i<=maxc ; i++){
+            for(int i= minc ; i<=maxc ; i++){
                 ans.push_back(arr[minr][i]);
             }
             minr++;
 
-            if(minr<=maxr && minc<=maxc){
-                for(int i = minr ; i<=maxr ; i++){
-                    ans.push_back(arr[i][maxc]);
-                }
-                maxc--;
+            if(minr>maxr || minc>maxc) break;
+            for(int i = minr ; i<=maxr ; i++){
+                ans.push_back(arr[i][maxc]);
             }
+            maxc--;
 
-            if(minr<=maxr && minc<=maxc){
-                for(int i = maxc ; i>=minc ; i--){
-                    ans.push_back(arr[maxr][i]);
-                }
-                maxr--;
+            if(minr>maxr || minc>maxc) break;
+            for(int i = maxc ; i>=minc ; i--){
+                ans.push_back(arr[maxr][i]);
             }
+            maxr--;
 
-            if(minr<=maxr && minc<=maxc){
-                for(int i = maxr ; i>=minr ; i--){
-                    ans.push_back(arr[i][minc]);
-                }
-                minc++;
+            if(minr>maxr || minc>maxc) break;
+            for(int i = maxr ; i>= minr ; i--){
+                ans.push_back(arr[i][minc]);
             }
+            minc++;
         }
-
         return ans;
     }
 };
