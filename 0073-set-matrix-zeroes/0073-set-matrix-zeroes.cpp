4d80a8bcc0d0fb2b866ad82841e9c66617887e51
Adarsh Seth <adarshofficial918@gmail.com>
2026-08-11 13:52:52 +0530

    Time: 0 ms (100%), Space: 20.8 MB (81.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& arr) {
         int m = arr.size();
-        int n = arr[0].size();
+        int n  = arr[0].size();
+        vector<bool> row(m,false);
+        vector<bool> col(n,false);
 
-        vector<bool> row(m , false);
-        vector<bool> col(n , false);
-
-        for(int i = 0 ; i<m ; i++){
-            for(int j = 0 ; j<n ; j++){
+        for(int i =0 ; i<m  ; i++){
+            for(int j = 0  ; j<n ; j++){
                 if(arr[i][j] == 0){
                     row[i] = true;
                     col[j] = true;
@@ -16,7 +15,7 @@ public:
             }
         }
 
-        for(int i = 0 ; i<m ; i++){
+        for(int i = 0 ; i<row.size() ; i++){
             if(row[i] == true){
                 for(int j = 0 ; j<n ; j++){
                     arr[i][j] = 0;
@@ -24,9 +23,9 @@ public:
             }
         }
 
-        for(int j = 0 ; j<n ; j++){
+        for(int j = 0  ; j<col.size() ; j++){
             if(col[j] == true){
-                for(int i = 0 ; i<m ; i++){
+                for(int i = 0  ; i<m ; i++){
                     arr[i][j] = 0;
                 }
             }
