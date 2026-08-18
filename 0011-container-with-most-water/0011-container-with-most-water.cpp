1b4f871cc9e806efc77d6ac7e457050d6e02d5b4
Adarsh Seth <adarshofficial918@gmail.com>
2026-08-18 20:46:10 +0530

    Time: 4 ms (20.56%), Space: 62.8 MB (79.37%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -5,12 +5,8 @@ public:
         int i = 0 , j = n-1;
         int maxArea = 0;
         while(i<j){
-            int h = min(arr[i], arr[j]);
-            int w = j-i;
-            int area = h*w;
-            if(area > maxArea){
-                maxArea = area;
-            }
+            int h = min(arr[i], arr[j]), w= j-i;
+            maxArea = max(h*w , maxArea);
             if(arr[i] <arr[j]) i++;
             else j--;
         }
