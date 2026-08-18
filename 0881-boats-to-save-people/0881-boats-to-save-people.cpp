c29807ab789e7806a7782122e60623d3c19b22be
Adarsh Seth <adarshofficial918@gmail.com>
2026-08-18 20:20:47 +0530

    Time: 19 ms (48.17%), Space: 45.9 MB (38.98%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -5,21 +5,15 @@ public:
         sort(people.begin(), people.end());
         int i = 0, j= n-1;
         int count = 0;
-        while(i<=j){
+        while(i<j){
             if(people[i] + people[j] <= limit){
                 count++;
                 i++;
                 j--;
             }
             else{
-                if(people[i]>people[j]){
-                    count++;
-                    i++;
-                }
-                else{
-                    count++;
-                    j--;
-                }
+                count++;
+                j--;
             }
         }
         if(i == j){
