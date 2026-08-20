371bc51a0d88bc38ae63ef898a95337e0d95ebb1
Adarsh Seth <adarshofficial918@gmail.com>
2026-08-20 10:11:32 +0530

    Time: 0 ms (100%), Space: 26.3 MB (36.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,20 +5,17 @@ public:
         if(n==1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
-        int lo =1, hi = n-2;
+        int lo =0, hi = n-1;
         while(lo<=hi){
             int mid = lo+(hi-lo)/2;
             if(nums[mid]!= nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
-            else if(nums[mid] != nums[mid-1] && nums[mid] == nums[mid+1]){
-                int left = mid, right = n-mid-2;
-                if(left%2 == 0) lo = mid+1;
-                else if(right%2 == 0) hi = mid-1;
-            }
-            else if(nums[mid] == nums[mid-1] && nums[mid] != nums[mid+1]){
-                int left = mid-1, right = n-mid-1;
-                if(left%2 == 0) lo = mid+1;
-                else if(right%2 == 0) hi = mid-1;
-            }
+
+            int f= mid, s = mid; // f is first mid, s is second mid
+            
+            if(nums[mid] == nums[mid-1]) f = mid-1;
+            else s = mid+1;
+            if((f-lo)%2 == 1) hi = f-1;
+            else lo = s+1;
         }
         return 3932;
     }
