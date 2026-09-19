c2ee590864fc7f51184f2801d17aec2b81193bc1
Adarsh Seth <adarshofficial918@gmail.com>
2026-09-19 18:30:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.81%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,7 +3,7 @@ public:
     double pow(double a, long long b){
         if(b==0) return 1;
         double p = pow(a,b/2);
-        return (b%2==0)? p*p : p*p*a;
+        return (b%2==0) ? p*p : p*p*a;
     }
     double myPow(double a, long long b) {
         bool flag = (b>=0);
